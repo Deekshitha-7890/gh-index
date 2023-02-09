183f1e6cced235c4d1c66981b8912a795b4629b1
Deekshitha-7890 <124881987+Deekshitha-7890@users.noreply.github.com>
2023-02-09 17:07:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -142,10 +142,10 @@ window.addEventListener('DOMContentLoaded', function () {
     updateIndexies: function updateIndexies(tree) {
       // generate header html
       var path = window.location.hash.replace('#', '');
-      var header = '<div class="header"><span>•_•</span></div>';
+      var header = '<div class="header"><span>Build Versions</span></div>';
       var parentLink = '#' + path.replace(/[^/]+\/$/, '');
       if (path) {
-        header = '<div class="header"><a class="uplink" href="' + parentLink + '">' + (path + '<span>←_←</span></a></div>');
+        header = '<div class="header"><a class="uplink" href="' + parentLink + '">' + (path + '<span>(previous)</span></a></div>');
       }
 
       // generate list html
